475b7893cc81f71ddf7d661c4a01393bcf5e5e6f
JosephYusufov <jyusufov00@stuy.edu>
2019-11-20 12:40:22 -0500

    working API

--- app.py
+++ app.py
@@ -94,22 +94,25 @@ def logout():
 
 @app.route("/lookup")
 def lookup():
-	r = urllib.request.urlopen(
-		"https://apps.bea.gov/api/data/?&UserID=1B07B684-579E-4E91-8517-DA093A82DA43&method=GetData&datasetname=Regional&TableName=SAINC1&GeoFIPS=STATE&LineCode=3&Year=2018&ResultFormat=JSON"  # Some API link goes here
-	)
-	data = json.loads(r.read())
+	if 'username' in session:
+		if request.args:
+			dothis()
+		r = urllib.request.urlopen(
+			"https://apps.bea.gov/api/data/?&UserID=1B07B684-579E-4E91-8517-DA093A82DA43&method=GetData&datasetname=Regional&TableName=SAINC1&GeoFIPS=STATE&LineCode=3&Year=2018&ResultFormat=JSON"  # Some API link goes here
+		)
+		data = json.loads(r.read())
+		print(data['BEAAPI']['Results']['Data'][1]['DataValue'])
+		# for member in data:
+			# print(member + "\n")
+		# CACHING MUST BE DONE WITH Flask-Caching
 
-	for member in data:
-		print(member + "\n")
-	# CACHING MUST BE DONE WITH Flask-Caching
-
-	# session['IncomeCache'] = data
-	# caches data to the data/ directory
-	# with open('./data/income.json', 'w') as outfile:
-	# 	json.dump(data, session['IncomeCache'], indent=4)
-	# print(data['results'][0]['name'])
-	return render_template("lookup.html", data=data)
-
+		# session['IncomeCache'] = data
+		# caches data to the data/ directory
+		# with open('./data/income.json', 'w') as outfile:
+		# 	json.dump(data, session['IncomeCache'], indent=4)
+		# print(data['results'][0]['name'])
+		return render_template("lookup.html", data=data['BEAAPI']['Results']['Data'])
+	return redirect("/login")
 
 if __name__ == "__main__":
 	app.debug = True
